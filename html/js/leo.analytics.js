70ca4db8f7c0cc0c1e1aa4b6372ab423e4f3192d
lzx <liuzhexiao.thunder@gmail.com>
2015-04-15 00:03:21 +0800

    update

---
+++ html/js/leo.analytics.js
@@ -0,0 +1,17 @@
+//百度统计
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?ec37f7cac2054ed4f08570483ffe2255";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+//Google Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-56953225-2', 'auto');
+ga('send', 'pageview');
+        
