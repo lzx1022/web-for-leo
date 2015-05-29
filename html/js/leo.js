3809a122e58743223b83d192944dc9586a27a452
lzx <liuzhexiao.thunder@gmail.com>
2015-05-29 23:17:53 +0800

    add leoHighlightNav()

--- html/js/leo.js
+++ html/js/leo.js
@@ -1,35 +1,27 @@
 // 目录:
 //     1.函数定义：
-//     1.highlightNav()
-//     2.lzxSlider()            图片轮播插件
+//          1. leoHighlightNav()
+//          2. leoSlider()            图片轮播插件
+//          3. leoHoverProject()
 
 //     2.载入所有函数
-function highlightNav() {
-    var nav = document.getElementsByTagName("nav");
-    if (nav.length == 0) return false;
-    var links = nav[0].getElementsByTagName("a"),
-        linkurl, lowerlinkurl;
-    for (var i = links.length - 1; i >= 0; i--) {
-        linkurl = links[i].textContent.toLowerCase() + "/";
-        if (window.location.href.indexOf(linkurl) != -1) {
-            links[i].className = "here";
-        }
-    }
-};
 
-function highlightNav_2() {
+// 1.1. highlightNav()
+function leoHighlightNav() {
+    var url = window.location.href;
     if ($("nav").length > 0) {
-        for (var i = 0; i < $("nav a").length; i++) {
-            var
-            if (window.location.href.indexOf($("nav a").text()) != -1) {
-                $("nav a")[i]
-            };
-        };
-    }
+        $("nav a").each(function() {
+            var a_text = $(this).text().toLowerCase(),
+            // 加上 "/" 防止本地测试的时候有其他字符串干扰
+                a_text_str = a_text.split(" ").join("-") + "/";
+            if (url.indexOf(a_text_str) != -1) {
+                $(this).addClass("here")
+        }
+    })} else return false;
 }
 
-
-function lzxSlider() {
+// 1.2. lzxSlider()
+function leoSlider() {
     if ($(".main-enter-page").length > 0) {
         $(".slideBox").slide({
             mainCell: ".bd ul",
@@ -55,6 +47,12 @@ function lzxSlider() {
     } else return false;
 }
 
+// 1.3. hoverProject()
+function leoHoverProject() {
+
+}
+
 $(function() {
-    lzxSlider();
+    leoHighlightNav();
+    leoSlider();
 })
